337c33ae2058b5a8436bf8ae1f7d3be161fe77d8
oluwasmart <lebioluwasoji@gmail.com>
2023-02-05 09:49:59 -0800

    airbnb updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
-"""
+"""starts a Flask web application"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
-
-app = Flask(__name__)
+from models.state import State
+app = Flask(__name__, template_folder='templates')
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    """display list of states"""
+    return render_template('7-states_list.html', 
+                           states=storage.all(State).values())
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def remove_session(response_or_exc):
+    """Remove the surrent session"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
